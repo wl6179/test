72b3293046446c3063ef9287f5cb7409d221382a
Chris Wang <44533122@qq.com>
2015-11-10 16:11:30 +0800

    测试数据

--- secache/demo/test1.php
+++ secache/demo/test1.php
@@ -1,15 +1,16 @@
 <?php
 require('../secache/secache.php');
-	$cache = new secache;
-	$cache->workat('cachedata');
 
-	$key = md5('test'); //必须自己做hash，前4位是16进制0-f,最长32位。
-	$value = '值数据'; //必须是字符串
+$cache = new secache;
+$cache->workat('cachedata');
 
-	$cache->store($key,$value);
+$key = md5('test');		//必须自己做hash，前4位是16进制0-f,最长32位。
+$value = '值数据';		//必须是字符串
 
-	if($cache->fetch($key,$return)){
-	    echo '<li>'.$key.'=>'.$return.'</li>';
-	}else{
-	    echo '<li>Data get failed! <b>'.$key.'</b></li>';
-	}
+$cache->store($key, $value);
+
+if($cache->fetch($key, $return)){
+	echo '<li>' . $key . '=>' . $return . '</li>';
+}else{
+	echo '<li>Data get failed! <b>' . $key . '</b></li>';
+}
