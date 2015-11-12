414d4d8d72804003510f72f492429ae90b444db5
Chris Wang <44533122@qq.com>
2015-11-13 01:51:06 +0800

    升级memcached的测试代码

--- memcache/onetest.php
+++ memcache/onetest.php
@@ -2,7 +2,7 @@
 header("Content-type:text/html;charset=utf-8");
 $mc = new Memcache();
 $mc->connect('192.168.0.233', 11211);
-
+//var_dump(MEMCACHE_COMPRESSED );
 $uid = (int)$_GET['uid'];
 if ($uid>0){
     $sql = "SELECT * FROM user WHERE uid='$uid'";
@@ -10,20 +10,34 @@ if ($uid>0){
     
     //当Mc没有此缓存数据时，走MySQL
     if (!($datas = $mc->get($key))) {   //将会从Mc取得一个完整的stdClass对象！！！~[即：Mc可以完整的将对象类型存储进Memory中]
+        
+        echo $key . ' - MySQL is working for this data（缓存无此数据）~';
+        
         $conn = mysql_connect('192.168.0.233', 'root', '666666');
         mysql_select_db('test');
         mysql_set_charset('utf8mb4');   //*
         $result = mysql_query($sql);
+        
         while ($row = mysql_fetch_object($result)){ //是一个stdClass对象！
             $datas[] = $row;
         }
-        $mc->add($key, $datas);     //$datas是一个大数组对象！
-        echo $key . '没有缓存数据，正在写入Mc缓存ing...';
-        var_dump($datas);
+        
+        if ($mc->set($key, $datas, MEMCACHE_COMPRESSED, 5)){     //$datas是一个大数组对象！(用add会出现隐性问题，应该全用set)
+            echo '&正在写入Mc缓存ing...Write OK!';
+        }else {
+            echo '&正在写入Mc缓存ing...Write Error!';
+        }
+        echo '<br />[MySQL]';
+        var_dump($datas);   //数据类型来自MySQL.
+        
     }else {
     //仅走Mc $mc->get
-        echo $key . '已经有缓存数据！没走MySQL！！';
-        var_dump($datas);
+        
+        echo $key . ' - Memcached is working for this data in 5 second（缓存有此数据）~';
+                
+        echo '<br />[Memcached]';
+        var_dump($datas);   //数据类型来自Memcached.
+        
     }
 }
 ?>
